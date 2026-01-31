735505d3ddbb6e6c79ae8bba7dc9bbc01173251e
yc5424tl <jkboline@gmail.com>
2026-01-30 22:20:26 -0600

    Missed file

--- hr_core/middleware/htmx_exception.py
+++ hr_core/middleware/htmx_exception.py
@@ -1,16 +1,8 @@
-# hr_core/middleware.py
+# hr_core/middleware/htmx_exception.py
 
 
 """
-Project middleware.
-
-Contains:
-1) RequestIdMiddleware
-   - Ensures every request has a stable request_id
-   - Stores request_id in a contextvar and binds it to structlog contextvars
-   - Adds X-Request-ID header to every response
-
-2) HtmxExceptionMiddleware
+HtmxExceptionMiddleware
    - Converts common exceptions (404, PermissionDenied) into HTMX-friendly
      responses when HX-Request is true.
    - For HTMX:
@@ -27,28 +19,9 @@ from collections.abc import Callable
 
 from django.core.exceptions import PermissionDenied
 from django.http import Http404, HttpRequest, HttpResponse
-from structlog.contextvars import clear_contextvars
 
 from hr_common.utils.htmx_responses import likely_session_expired
-from hr_common.utils.unified_logging import (generate_request_id, REQUEST_ID_HEADER, REQUEST_ID_META_KEY, reset_request_id, set_request_id)
 from utils.http.htmx import is_htmx
-
-
-class RequestIdMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request: HttpRequest) -> HttpResponse:
-        request_id = request.META.get(REQUEST_ID_META_KEY) or generate_request_id()
-        token = set_request_id(request_id)
-        request.request_id = request_id
-        try:
-            resp = self.get_response(request)
-        finally:
-            clear_contextvars()
-            reset_request_id(token)
-        resp[REQUEST_ID_HEADER] = request_id
-        return resp
 
 
 class HtmxExceptionMiddleware:
