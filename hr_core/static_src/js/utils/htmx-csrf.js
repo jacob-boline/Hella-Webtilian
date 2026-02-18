a16bd0e203c60228ad6b8c88242060ae674ccdcd
yc5424tl <jkboline@gmail.com>
2026-02-18 15:26:35 -0600

    fixed export

--- hr_core/static_src/js/utils/htmx-csrf.js
+++ hr_core/static_src/js/utils/htmx-csrf.js
@@ -1,13 +1,13 @@
 // hr_core/static_src/js/utils/htmx-csrf.js
 
-export function getCsrfTokenFromCookie() {
+export function getCsrfToken() {
     const m = document.cookie.match(/(^|;\s*)csrftoken=([^;]+)/);
     return m ? decodeURIComponent(m[2]) : '';
 }
 
 (function () {
     document.body.addEventListener('htmx:configRequest', function (event) {
-        const token = getCsrfTokenFromCookie();
+        const token = getCsrfToken();
         if (token) {
             event.detail.headers['X-CSRFToken'] = token;
         }
